313906c70b04394fc9e2afc4ac604eda22abf546
CiberNefty <danieldbaproyects@gmail.com>
2022-12-13 17:37:48 -0500

    Modificamos el archivo de creacion de la BD por que recordemos que esta bd la creamos para utilizarse en Mysql wordbench, v2_BD_vinoteca

--- DB_vinoteca_sinRelaciones/creacion de vinoteca sin Relaciones.sql
+++ DB_vinoteca_sinRelaciones/creacion de vinoteca sin Relaciones.sql
@@ -5,7 +5,7 @@ SHOW TABLES;
 DROP TABLE IF exists vinos;
 
 CREATE TABLE IF NOT exists vinos(
-anaquel int unsigned NOT NULL auto_increment,
+anaquel serial NOT NULL,
 vino varchar(35) not null,
 productor varchar(25) not null,
 year int not null default '1900',
@@ -13,8 +13,9 @@ botellas int not null default '1',
 listo int not null,
 description text,
 PRIMARY KEY (anaquel)
-)ENGINE = InnoDB default char set=utf8mb4;
-
+);
+SELECT * FROM vinos;
+DELETE FROM vinos;
 INSERT INTO vinos(anaquel, vino, productor, year, botellas, listo) VALUES(2,'Chardonnay','Buena Vista', 1988, 1,1991);
 INSERT INTO vinos(anaquel,vino, productor, year, botellas, listo) VALUES (3,'Chardonnay','Louis Martino', 1989,5,1990);
 INSERT INTO vinos (anaquel,vino,productor,year,Botellas,listo,description) VALUES(6,'Chardonnay','Chappellet',1987,4,1991,'Dia de gracia');
@@ -25,7 +26,7 @@ VALUES (11,'Jo. Riesling','Jekel',1989,10,1992,null),
 (16,'Jo. Riesling','Sttui',1987,1,1989,'Muy seco');
 # ---------------------------------------------------------
 INSERT INTO vinos VALUES(21,'Fume Blanc','Ch. St. Jean',1988,4,1991,'Robt. Flores'),
-(22,"Fume Blanc",'Robt. Maldavi',1987,2,1990,null),
+(22,'Fume Blanc','Robt. Maldavi',1987,2,1990,null),
 (25,'Borgona blanco','Mirassou',1986,6,1989,null),
 (30,'Gewurztraminer','Buena Vista',1987,3,1990,null);
 INSERT INTO vinos VALUES(43,'Cab Sauvignon','Robt. Maldavi',1982,12,1992,null),
@@ -35,9 +36,4 @@ INSERT INTO vinos VALUES(43,'Cab Sauvignon','Robt. Maldavi',1982,12,1992,null),
 (64,'Zinfaldel','Mirassou',1984,9,1992,null);
 
 INSERT INTO vinos VALUES (80,'Merlot','Clos du Bois',1988, 12,1992,'Navidad'),
-(11,'Jo. Riesling','Jekel',1989,10,1992,null),
-(12,'Jo. Riesling','Buena Vista',1987,1,1992,'Cosecha tarde'),
-(43,'Cab Sauvignon','Robt. Maldavi',1982,12,1992,null),
-(51,'Pinot Noir','Ch. St. Jean',1986,2,1992,null),
-(64,'Zinfaldel','Mirassou',1984,9,1992,null),
 (53, 'Pinot Noir', 'Saintsbury', 1987, 1 , 1993, 'Para Navidad');

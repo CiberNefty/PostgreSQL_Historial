189cd03bf62888eec38824477f17e7db1687ed75
CiberNefty <danieldbaproyects@gmail.com>
2023-01-07 11:35:52 -0500

    Creamos una carpeta que se llama DB_Movies, y guardamos un backup para practicas y un historial sobree comandos avanzados

---
+++ DB_Movies_Blapzi/pgsql_cienciadatos.sql
@@ -0,0 +1,88 @@
+PUNTOS FUERTES DE LAS RDB
+
+* Multiproposito.
+* Ampliamente utilizadas.
+* Informacion consistente.
+* Flexible.
+* Retrocompatible.
+* Completamente programable.
+
+-------------------------------------
+ESTAMOS UTILIZANDO LA DB de MOVIES
+
+
+SELECT MAX(ultima_actualizacion) AS fecha_ultima_actualizacion,
+		clasificacion, COUNT(*) AS cantidad_peliculas
+FROM peliculas
+WHERE duracion_renta > 3
+GROUP BY clasificacion, ultima_actualizacion
+ORDER BY fecha_ultima_actualizacion;
+
+-- -----------------------------------
+CREATE OR REPLACE PROCEDURE test_dropcreate_procedure()
+LANGUAGE SQL
+AS $$
+	DROP TABLE  IF EXISTS aaa; -- Aquí estamos haciendo una pruba de borrar
+	CREATE TABLE aaa(bbb char(5) CONSTRAINT firstkey PRIMARY KEY);
+$$;
+
+-- Ahora para llamar nuestro procedimiento
+CALL test_dropcreate_procedure();	
+-- ------------------------------------
+CREATE OR REPLACE FUNCTION test_dropcreate_function()
+RETURNS VOID -- Aqui estamos diciendo que regresa un vacio
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	DROP TABLE IF EXISTS aaa;
+	CREATE TABLE aaa (bbb char(5) constraint firstkey PRIMARY KEY,
+					 ccc char(5));
+	DROP TABLE IF EXISTS aaab;
+	CREATE TABLE aaab (bbba char(5) constraint secondkey PRIMARY KEY,
+					 ccca char(5)); -- las llaves unicas deben ser diferentes todas las de la BD
+END;
+$$;
+
+-- Ahora una firma de llamar a las funciones es con SELECT
+-- Puede ser de la siguiente manera 
+-- SELECT * FROM test_dropcreate_function();
+SELECT test_dropcreate_function();
+
+-- -------------------------------
+CREATE OR REPLACE FUNCTION count_total_movies()
+RETURNS int
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	return COUNT(*) FROM peliculas;
+END;
+$$;
+
+SELECT count_total_movies();
+-- ------------------------
+
+CREATE OR REPLACE FUNCTION duplicate_records()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+-- La idea de esta funcion es tomar el registro que se inserto en la tabla y lo vamos a meter
+-- igual o transformado en otra tabla.
+BEGIN
+	INSERT INTO aaab(bbba,ccca)
+	VALUES(NEW.bbb, NEW.ccc);
+	RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER aaa_changes 
+-- Este nombre del trigger es para que se pamos que se ejecutara cuando cambia la tabla aaa
+BEFORE INSERT 
+ON aaa
+FOR EACH ROW
+--  Esta funcion es para que cada vez que ingrese un registro en la tabla aaa ingrese un registro igual en la table aaab
+EXECUTE PROCEDURE duplicate_records();
+
+INSERT INTO aaa (bbb,ccc) 
+VALUES ('abcde','efghi');
+
+SELECT * FROM aaab;
